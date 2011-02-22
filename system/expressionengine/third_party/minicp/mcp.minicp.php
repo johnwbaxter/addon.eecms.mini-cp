032b81d8a697727b73fa4c528e9a1aa36d5bb1ff
Benjamin David <contact@dukt.fr>
2011-02-22 15:51:59 +0100

    Fixed a bug where Module was not showing up in ExpressionEngine Add-Ons Modules list (mcp file is back) Fixed CSS bugs

---
+++ system/expressionengine/third_party/minicp/mcp.minicp.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine Mini CP Module
+ *
+ * @package			Mini CP
+ * @subpackage		Modules
+ * @category		Modules
+ * @author			Benjamin David
+ * @link			http://duktee.com/addons/mini-cp/ 
+ */
+
+class Minicp_mcp {
+
+	/* constructor*/
+	
+	function Minicp_mcp()
+	{
+		/* Make a local reference to the ExpressionEngine super object */
+		$this->EE =& get_instance();
+	}
+
+	
+}
+
+
+/* END Class */
+
+/* End of file mcp.minicp.php */
+/* Location: ./system/expressionengine/third_party/minicp/mcp.minicp.php */
